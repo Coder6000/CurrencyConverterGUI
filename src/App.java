f1347bc6cb948c7c169d492d43fe2c247ff07050
Coder6000 <dummy-github-email>
2024-03-23 11:23:54 +0100

    created initial window

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
-public class App {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class App{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        CreateWindow();
+    }
+
+    public static void CreateWindow(){
+        JFrame window = new JFrame();
+
+        window.setTitle("Currency Converter");
+        window.setSize(350, 350);
+        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+
+        window.setVisible(true);
     }
 }
