f4013dc27fed08946766a45c7ca8730f7a7d6c3c
Coder6000 <dummy-github-email>
2024-03-28 22:04:24 +0100

    Added the GUI Layout

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,21 +1,76 @@
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import java.awt.Font;
 
 public class MainWindow {
 
     private JFrame window = new JFrame();
     
+    private JLabel initialCurrencyText = new JLabel("Initial Currency: ");
+    private JLabel amountText = new JLabel("Amount: ");
+    private JLabel wantedCurrencyText = new JLabel("Wanted Currency: ");
+    private JLabel outputText = new JLabel("Output: ");
+
+    private JTextField amountField = new JTextField();
+    private JTextField outputField = new JTextField();
+
+    private String initialCurrencies[] = {"British Pounds", "Dollars", "Euros"};
+    private JComboBox initialCurrencyOptions = new JComboBox<>(initialCurrencies);
+
+    private String wantedCurrencies[] = {"British Pounds", "Dollars", "Euros"};
+    private JComboBox wantedCurrencyOptions = new JComboBox<>(wantedCurrencies);
+    
     public MainWindow(){
-        initialize();   
+        initializeWindow();
+        initializeLabels();
+
+        window.setVisible(true);
     }
 
-    private void initialize(){
+    private void initializeWindow(){
         window.setTitle("Currency Converter");
         window.setSize(350, 350);
         window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         window.setLocationRelativeTo(null);
         window.setLayout(null);
         window.setResizable(false);
-        
-        window.setVisible(true);
+    }
+
+    private void initializeLabels(){
+
+        //Initial Currency Section
+        initialCurrencyText.setBounds(3, 3, 120, 80);
+        initialCurrencyText.setFont(new Font("Arial Black", Font.PLAIN, 13));
+        window.add(initialCurrencyText);
+
+        initialCurrencyOptions.setBounds(140, 35, 110, 18);
+        window.add(initialCurrencyOptions);
+
+        amountText.setBounds(3, 30, 120, 80);
+        amountText.setFont(new Font("Arial Black", Font.PLAIN, 13));
+        window.add(amountText);
+
+        amountField.setBounds(70, 62, 120, 20);
+        window.add(amountField);
+
+        //Wanted Currency Section
+        wantedCurrencyText.setBounds(3, 100, 140, 80);
+        wantedCurrencyText.setFont(new Font("Arial Black", Font.PLAIN, 13));
+        window.add(wantedCurrencyText);
+
+        wantedCurrencyOptions.setBounds(140, 134, 110, 18);
+        window.add(wantedCurrencyOptions);
+
+        outputText.setBounds(3, 140, 70, 80);
+        outputText.setFont(new Font("Arial Black", Font.PLAIN, 13));
+        window.add(outputText);
+
+        outputField.setBounds(70, 171, 120, 20);
+        outputField.setEditable(false);
+        window.add(outputField);
     }
 }
