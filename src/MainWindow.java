8bd10deabd2a273beb71c28c831ba416ad8b4483
Coder6000 <dummy-github-email>
2024-04-01 20:10:19 +0200

    Added function to Convert Euro to other currencies

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,10 +1,14 @@
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import java.util.Objects;
 
 public class MainWindow {
 
@@ -15,18 +19,27 @@ public class MainWindow {
     private JLabel wantedCurrencyText = new JLabel("Wanted Currency: ");
     private JLabel outputText = new JLabel("Output: ");
 
-    private JTextField amountField = new JTextField();
-    private JTextField outputField = new JTextField();
+    private JTextField amountFieldInput = new JTextField();
+    private float initialAmount;
 
-    private String initialCurrencies[] = {"British Pounds", "Dollars", "Euros"};
+    private JTextField outputField = new JTextField();
+    private float wantedAmount;
+
+    private String initialCurrencies[] = {"Euros", "Dollars", "British Pounds"};
     private JComboBox initialCurrencyOptions = new JComboBox<>(initialCurrencies);
 
-    private String wantedCurrencies[] = {"British Pounds", "Dollars", "Euros"};
+    private String wantedCurrencies[] = {"Euros", "Dollars", "British Pounds"};
     private JComboBox wantedCurrencyOptions = new JComboBox<>(wantedCurrencies);
+
+    private JButton convertButton = new JButton("Convert");
+
+    private static final DecimalFormat df = new DecimalFormat("0.00");
     
     public MainWindow(){
         initializeWindow();
         initializeLabels();
+        initializeButtons();
+        initializeListeners();
 
         window.setVisible(true);
     }
@@ -54,8 +67,8 @@ public class MainWindow {
         amountText.setFont(new Font("Arial Black", Font.PLAIN, 13));
         window.add(amountText);
 
-        amountField.setBounds(70, 62, 120, 20);
-        window.add(amountField);
+        amountFieldInput.setBounds(70, 62, 120, 20);
+        window.add(amountFieldInput);
 
         //Wanted Currency Section
         wantedCurrencyText.setBounds(3, 100, 140, 80);
@@ -72,5 +85,52 @@ public class MainWindow {
         outputField.setBounds(70, 171, 120, 20);
         outputField.setEditable(false);
         window.add(outputField);
+    }
+
+    private void initializeButtons(){
+        convertButton.setBounds(220, 200, 100, 40);
+        window.add(convertButton);
+    }
+
+    private void initializeListeners() {
+        convertButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                ConvertEuro();
+            }
+        });
+    }
+
+    public void ConvertEuro()
+    {
+        // initialAmount = Float.parseFloat(amountFieldInput.getText());
+        if(Objects.equals(String.valueOf(initialCurrencyOptions.getSelectedIndex()), "0")){
+
+            // From Euro to Euro
+            if(Objects.equals(String.valueOf(wantedCurrencyOptions.getSelectedIndex()), "0")){
+                initialAmount = Float.parseFloat(amountFieldInput.getText());
+
+                wantedAmount = initialAmount * 1;
+
+                outputField.setText(String.valueOf(df.format(wantedAmount)));
+            }
+
+            //From Euro to Dollar
+            if(Objects.equals(String.valueOf(wantedCurrencyOptions.getSelectedIndex()), "1")){
+                initialAmount = Float.parseFloat(amountFieldInput.getText());
+
+                wantedAmount = initialAmount * 1.08f;
+
+                outputField.setText(String.valueOf(df.format(wantedAmount)));
+            }
+
+            //From Euro to British Pounds
+            if(Objects.equals(String.valueOf(wantedCurrencyOptions.getSelectedIndex()), "2")){
+                initialAmount = Float.parseFloat(amountFieldInput.getText());
+
+                wantedAmount = initialAmount * 0.86f;
+
+                outputField.setText(String.valueOf(df.format(wantedAmount)));
+            }
+        }
     }
 }
